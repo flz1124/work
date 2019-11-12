3a7d16696d1f68aaa73fb501bd8d0b3f2b479e2d
flz <912354886@qq.com>
2019-11-11 17:38:23 -0800

    work10

---
+++ work10/test1.c
@@ -0,0 +1,24 @@
+#include "my.h"
+static void fun1(void)
+{
+	printf("invoked callbackfun!\n");
+}
+int main()
+{
+	atexit(fun1);
+	FILE *fp;
+	char buf[] = "buffer data!";
+	if((fp = fopen("test.dat" , "w+")) == NULL)
+	{
+		perror("creat file failed!\n");
+		exit(-1);
+	}
+	if(fputs(buf,fp) == EOF)
+	{
+		perror("fputs file failed!\n");
+		exit(-1);
+	}
+	printf("hello   ");
+	exit(0);
+	//_exit(0);
+}
